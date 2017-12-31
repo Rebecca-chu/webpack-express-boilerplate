eeeb308ca3bfaafc6b499a220e8dce9131843533
Rebecca <rebecca.chu518@gmail.com>
2017-12-31 21:45:53 +0800

    Update constants to const file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dailyApp from './routes';
+import { SERVER_PORT } from './constants';
 
 const app = express();
 
 app.use(dailyApp);
 
-app.listen(3000, () => {
-  console.log('Daily app is listening on port 3000!');
+app.listen(SERVER_PORT, () => {
+  console.log(`Daily app is listening on port ${SERVER_PORT}!`);
 });
