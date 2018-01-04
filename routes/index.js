ba3dc84e1598a1a31573674d844f44ad1ae0dff2
Rebecca <rebecca.chu518@gmail.com>
2018-01-04 21:08:42 +0800

    Move webpack build directory

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ import path from 'path';
 
 const dailyApp = express.Router();
 
-dailyApp.use(express.static(path.join(__dirname, '/../public')));
+dailyApp.use(express.static(path.join(__dirname, '/../dist')));
 
 export default dailyApp;
